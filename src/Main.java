84a4f135ecf23430117fbc1e0227c000ea734210
Karol Siwek <siwek.karol1992@gmail.com>
2018-10-02 19:27:20 +0200

    huuggyug

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Product product1 = new Product("mleko", 2.5);
+        Product product2 = new Product("mleko", 2.5);
+
+        if (product1.equals(product2)) {
+            System.out.println("Produkty równe");
+
+        } else {
+            System.out.println("Produkty różne");
+        }
+
+//        String str1 = new String("Ania");
+//        String str2 = new String("Ania");
+//
+//        if (str1.equals(str2)) {
+//            System.out.println("Napisy równe");
+//
+//        } else {
+//            System.out.println("Napisy różne");
+//        }
+    }
+}
